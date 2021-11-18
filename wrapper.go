babdc65defd0a09708cf91242f25b246acb9e674
Erik Unger <erik@erikunger.com>
2021-11-18 10:28:58 +0100

    more elaborate wrapper todo usage panic

--- wrapper.go
+++ wrapper.go
@@ -2,12 +2,7 @@ package function
 
 import (
 	"context"
-)
-
-var (
-	_ CallWrapper                 = CallWrapperFunc(nil)
-	_ CallWithStringsWrapper      = CallWithStringsWrapperFunc(nil)
-	_ CallWithNamedStringsWrapper = CallWithNamedStringsWrapperFunc(nil)
+	"reflect"
 )
 
 type Wrapper interface {
@@ -17,18 +12,57 @@ type Wrapper interface {
 	CallWithNamedStringsWrapper
 }
 
+func WrapperTODO(function interface{}) Wrapper {
+	if reflect.ValueOf(function).Kind() != reflect.Func {
+		panic("function.WrapperTODO must be used with a function as argument, then run gen-func-wrappers to to replace it with generated code")
+	}
+	panic("function.WrapperTODO: run gen-func-wrappers")
+}
+
 type CallWrapper interface {
 	Call(ctx context.Context, args []interface{}) (results []interface{}, err error)
 }
+
+func CallWrapperTODO(function interface{}) CallWrapper {
+	if reflect.ValueOf(function).Kind() != reflect.Func {
+		panic("function.CallWrapperTODO must be used with a function as argument, then run gen-func-wrappers to to replace it with generated code")
+	}
+	panic("function.CallWrapperTODO: run gen-func-wrappers")
+}
+
+type CallWithStringsWrapper interface {
+	CallWithStrings(ctx context.Context, args ...string) (results []interface{}, err error)
+}
+
+func CallWithStringsWrapperTODO(function interface{}) CallWithStringsWrapper {
+	if reflect.ValueOf(function).Kind() != reflect.Func {
+		panic("function.CallWithStringsWrapperTODO must be used with a function as argument, then run gen-func-wrappers to replace it with generated code")
+	}
+	panic("function.CallWithStringsWrapperTODO: run gen-func-wrappers")
+}
+
+type CallWithNamedStringsWrapper interface {
+	CallWithNamedStrings(ctx context.Context, args map[string]string) (results []interface{}, err error)
+}
+
+func CallWithNamedStringsWrapperTODO(function interface{}) CallWithNamedStringsWrapper {
+	if reflect.ValueOf(function).Kind() != reflect.Func {
+		panic("function.CallWithNamedStringsWrapperTODO must be used with a function as argument, then run gen-func-wrappers to replace it with generated code")
+	}
+	panic("function.CallWithNamedStringsWrapperTODO: run gen-func-wrappers")
+}
+
+// Implementations of the call interfaces as higher order functions
+var (
+	_ CallWrapper                 = CallWrapperFunc(nil)
+	_ CallWithStringsWrapper      = CallWithStringsWrapperFunc(nil)
+	_ CallWithNamedStringsWrapper = CallWithNamedStringsWrapperFunc(nil)
+)
 
 type CallWrapperFunc func(ctx context.Context, args []interface{}) (results []interface{}, err error)
 
 func (f CallWrapperFunc) Call(ctx context.Context, args []interface{}) (results []interface{}, err error) {
 	return f(ctx, args)
-}
-
-type CallWithStringsWrapper interface {
-	CallWithStrings(ctx context.Context, args ...string) (results []interface{}, err error)
 }
 
 type CallWithStringsWrapperFunc func(ctx context.Context, args ...string) (results []interface{}, err error)
@@ -37,28 +71,8 @@ func (f CallWithStringsWrapperFunc) CallWithStrings(ctx context.Context, args ..
 	return f(ctx, args...)
 }
 
-type CallWithNamedStringsWrapper interface {
-	CallWithNamedStrings(ctx context.Context, args map[string]string) (results []interface{}, err error)
-}
-
 type CallWithNamedStringsWrapperFunc func(ctx context.Context, args map[string]string) (results []interface{}, err error)
 
 func (f CallWithNamedStringsWrapperFunc) CallWithNamedStrings(ctx context.Context, args map[string]string) (results []interface{}, err error) {
 	return f(ctx, args)
-}
-
-func WrapperTODO(f interface{}) Wrapper {
-	panic("function.WrapperTODO: run gen-func-wrappers")
-}
-
-func CallWrapperTODO(f interface{}) CallWrapper {
-	panic("function.CallWrapperTODO: run gen-func-wrappers")
-}
-
-func CallWithStringsWrapperTODO(f interface{}) CallWithStringsWrapper {
-	panic("function.CallWithStringsWrapperTODO: run gen-func-wrappers")
-}
-
-func CallWithNamedStringsWrapperTODO(f interface{}) CallWithNamedStringsWrapper {
-	panic("function.CallWithNamedStringsWrapperTODO: run gen-func-wrappers")
 }
