a80f9a628ac9ec6b7697ae98c8b2d32e3099480b
Erik Unger <erik@erikunger.com>
2022-01-10 10:52:03 +0100

    added HTTPHandlerNoWrapper

--- httphandler.go
+++ httphandler.go
@@ -1,6 +1,7 @@
 package function
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/ungerik/go-httpx/httperr"
@@ -42,6 +43,43 @@ func HTTPHandler(getArgs HTTPRequestArgsGetter, function CallWithNamedStringsWra
 		results, err := function.CallWithNamedStrings(request.Context(), args)
 		if resultsWriter != nil {
 			err = resultsWriter.WriteResults(results, err, writer, request)
+		}
+		if err == nil {
+			return
+		}
+
+		if len(errHandlers) == 0 {
+			httperr.Handle(err, writer, request)
+		} else {
+			for _, errHandler := range errHandlers {
+				errHandler.HandleError(err, writer, request)
+			}
+		}
+	}
+}
+
+// HTTPHandlerNoWrapper returns an http.Handler for a function without a wrapper
+// of type func(context.Context) ([]byte, error) that returns response bytes.
+func HTTPHandlerNoWrapper(getArgs HTTPRequestArgsGetter, function func(context.Context) ([]byte, error), resultsWriter HTTPResultsWriter, errHandlers ...httperr.Handler) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		if CatchHTTPHandlerPanics {
+			defer func() {
+				if p := recover(); p != nil {
+					err := httperr.AsError(p)
+					if len(errHandlers) == 0 {
+						httperr.Handle(err, writer, request)
+					} else {
+						for _, errHandler := range errHandlers {
+							errHandler.HandleError(err, writer, request)
+						}
+					}
+				}
+			}()
+		}
+
+		result, err := function(request.Context())
+		if resultsWriter != nil {
+			err = resultsWriter.WriteResults([]interface{}{result}, err, writer, request)
 		}
 		if err == nil {
 			return
