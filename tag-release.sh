dde8e765ae3efee16f6e3fd1544b035a10a2b2f8
Erik Unger <erik@erikunger.com>
2025-05-09 12:46:54 +0200

    improved release script

--- tag-release.sh
+++ tag-release.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-SEMVER_REGEX='^v([0-9]+)\.([0-9]+)\.([0-9]+)(-[a-zA-Z0-9]+)?$'
+# Just in case the script is run from another directory
+SCRIPT_DIR=$(cd -P -- $(dirname -- "$0") && pwd -P)
+cd $SCRIPT_DIR
 
+MODULE_PATHS=("" "cli/" "htmlform/" "cmd/gen-func-wrappers/")
+
+SEMVER_REGEX='^v([0-9]+)\.([0-9]+)\.([0-9]+)(-[a-zA-Z0-9]+)?$'
 if [[ ! "$1" =~ $SEMVER_REGEX ]]; then
     echo "Usage: $0 <version> [message]"
     exit 1
@@ -9,18 +14,15 @@ fi
 
 VERSION=$1
 MESSAGE="$VERSION"
-
-# Check if a message was provided as the second argument
 if [ -n "$2" ]; then
-    MESSAGE="$2"
+    MESSAGE="$2" # provided as the second argument
 fi
 
 echo "Tagging $VERSION with message: '$MESSAGE'"
 
-git tag -a $VERSION -m "$MESSAGE"
-git tag -a cli/$VERSION -m "$MESSAGE"
-git tag -a htmlform/$VERSION -m "$MESSAGE"
-git tag -a cmd/gen-func-wrappers/$VERSION -m "$MESSAGE"
+for PREFIX in "${MODULE_PATHS[@]}"; do
+    git tag -a "${PREFIX}${VERSION}" -m "$MESSAGE"
+done
 
 echo "Tags to be pushed:"
 git push --tags --dry-run
@@ -30,9 +32,8 @@ read CONFIRM
 if [[ "$CONFIRM" == "y" || "$CONFIRM" == "Y" ]]; then
     git push origin --tags
 else
-    git tag -d $VERSION
-    git tag -d cli/$VERSION
-    git tag -d htmlform/$VERSION
-    git tag -d cmd/gen-func-wrappers/$VERSION
+    for PREFIX in "${MODULE_PATHS[@]}"; do
+        git tag -d "${PREFIX}${VERSION}"
+    done
     echo "Reverted local $VERSION tags"
 fi
